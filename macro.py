c02fae39408e91bdadb6e34623d03dc85f56ea8f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-26 22:40:22 +0000

    button shortcuts implemented

--- macro.py
+++ macro.py
@@ -20,31 +20,35 @@ except:
 
 class ui:
 	def __init__(self):
+		self.record_macrokey = "."
+		self.execute_macrokey = ","
 		self.main()
 
 	def main(self):
 		root = Tk()
 		root.title("macro's")
 		root.geometry("400x400")
+		root.bind(self.record_macrokey, record)
+		root.bind(self.execute_macrokey, execute)
 
-		recordmacrobtn = tk.Button(root, width=20, height=2, text="Record Macro",  fg="white", bg="#263D42", command=record_macro())
+		recordmacrobtn = tk.Button(root, width=20, height=2, text="Record Macro",  fg="white", bg="#263D42", command=record)
 		recordmacrobtn.grid()
 
-		executemacrobtn = tk.Button(root, width=20, height=2, text="Execute Macro",  fg="white", bg="#263D42", command=record_macro())
+		executemacrobtn = tk.Button(root, width=20, height=2, text="Execute Macro",  fg="white", bg="#263D42", command=execute)
 		executemacrobtn.grid()
 
 		mainloop()
 
-def record_macro():
+def record(event=""):
 	print("record macro function ran")
 
-def save_macro():
+def save():
 	print("save macro function ran")
 	
-def execute_macro():
+def execute(event=""):
 	print("execute macro function ran")
 
-def load_macro(file):
+def load(file):
 	print("load macro function ran")
 
 if __name__ == "__main__":
