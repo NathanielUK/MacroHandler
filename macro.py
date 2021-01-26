6d01b64c3a96b6ead99673900fbc6fb7e0c7510f
NathanielUK <nathanbarron2k03@gmail.com>
2021-01-26 23:55:36 +0000

    need to record mouse/keyboard and handle data on execution

--- macro.py
+++ macro.py
@@ -9,48 +9,76 @@ try:
 	
 	import threading
 	from threading import Thread
-	
-	from pynput.keyboard import Key, Controller, Listener
-	from pynput.mouse import Button, Controller
-	keyboard = Controller()
-	mouse = Controller()
+
+	import mouse
+	import keyboard
 
 except:
 	print("trouble installing packages")
 
 class ui:
 	def __init__(self):
-		self.record_macrokey = "."
-		self.execute_macrokey = ","
+		self.record_macrokey = "x"
+		self.stoprecord_macrokey = "."
+		self.execute_macrokey = "z"
+		self.stopexecute_macrokey = ","
+		self.eventlist = []
+		self.executelist = []
 		self.main()
 
 	def main(self):
 		root = Tk()
 		root.title("macro's")
 		root.geometry("400x400")
-		root.bind(self.record_macrokey, record)
-		root.bind(self.execute_macrokey, execute)
+		root.bind(self.record())
+		root.bind(self.execute())
 
-		recordmacrobtn = tk.Button(root, width=20, height=2, text="Record Macro",  fg="white", bg="#263D42", command=record)
+		recordmacrobtn = tk.Button(root, width=20, height=2, text="Record Macro",  fg="white", bg="#263D42", command=self.record)
 		recordmacrobtn.grid()
 
-		executemacrobtn = tk.Button(root, width=20, height=2, text="Execute Macro",  fg="white", bg="#263D42", command=execute)
+		executemacrobtn = tk.Button(root, width=20, height=2, text="Load Macro",  fg="white", bg="#263D42", command=self.load)
+		executemacrobtn.grid()
+
+		executemacrobtn = tk.Button(root, width=20, height=2, text="Execute Macro",  fg="white", bg="#263D42", command=self.execute)
 		executemacrobtn.grid()
 
 		mainloop()
 
-def record(event=""):
-	print("record macro function ran")
+	def record(self, event=""):
 
-def save():
-	print("save macro function ran")
+		print("record macro function ran")
+
+		# log mouse/keyboard action
+
+		filename = "test.txt"
+		self.save(filename)
+
+
+	def save(self, filename):
+		print("save macro function ran")
+
+		# test inputs
+		self.eventlist = ["g","a","y"]
+
+		# write input to txt
+		file = open(filename, "w")
+		for line in self.eventlist:
+			file.write(line + "\n")
+
+	def load(self, file=""):
+		print("load macro function ran")
+
+		from tkinter.filedialog import askopenfile
+		file = askopenfile(mode="r")
+		if file is not None:
+			content = file.read()
+			for instruction in content:
+				self.executelist.append(instruction)
+
+	def execute(self, event=""):
+		print("execute macro function ran")
+		# handle instructions in self.executelist
+
 	
-def execute(event=""):
-	print("execute macro function ran")
-
-def load(file):
-	print("load macro function ran")
-
 if __name__ == "__main__":
 	ui()
-
